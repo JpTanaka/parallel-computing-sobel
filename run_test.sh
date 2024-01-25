f1240aba5786c8bd2a78c2ce3bacb067c2517d41
Joao Tanaka Montalvao <joao.tanaka-montalvao@allemagne.polytechnique.fr>
2024-01-25 14:50:51 +0100

    added plot chart to run_test

--- run_test.sh
+++ run_test.sh
@@ -5,6 +5,13 @@ make
 INPUT_DIR=images/original
 OUTPUT_DIR=images/processed
 mkdir $OUTPUT_DIR 2>/dev/null
+input_file="perf.log"
+
+
+if [ -f "$input_file" ]; then
+    rm "$input_file"
+    touch "$input_file"    
+fi 
 
 for i in $INPUT_DIR/*gif ; do
     DEST=$OUTPUT_DIR/`basename $i .gif`-sobel.gif
@@ -12,3 +19,14 @@ for i in $INPUT_DIR/*gif ; do
 
     ./sobelf $i $DEST
 done
+
+
+if [ ! -f "$input_file" ]; then
+    echo "Error: File $input_file not found."
+    exit 1
+fi
+
+temp_file=$(mktemp)
+sort -k3,3 -n "$input_file" > "$temp_file"
+mv "$temp_file" "$input_file"
+gnuplot plot.gp > plot.png
